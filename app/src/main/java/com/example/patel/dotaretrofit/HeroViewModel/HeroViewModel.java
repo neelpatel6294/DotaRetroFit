d6c79304b48a8515a0ca2628e8d61bce1c0b09a0
Neel Patel <neelpatel6294@gmail.com>
2018-08-27 09:57:50 -0400

    ViewModel updated2

--- app/src/main/java/com/example/patel/dotaretrofit/HeroViewModel/HeroViewModel.java
+++ app/src/main/java/com/example/patel/dotaretrofit/HeroViewModel/HeroViewModel.java
@@ -3,6 +3,7 @@ package com.example.patel.dotaretrofit.HeroViewModel;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 
 import com.example.patel.dotaretrofit.HeroRepository;
@@ -13,15 +14,34 @@ import java.util.List;
 public class HeroViewModel extends AndroidViewModel {
 
     private LiveData<List<Heroes>> mData;
+    private LiveData<List<Heroes>> mDataFav;
+
     private HeroRepository mRepository;
 
     public HeroViewModel(@NonNull Application application) {
         super(application);
         mRepository = new HeroRepository(application);
-        mData = mRepository.mLiveData();
     }
 
     public LiveData<List<Heroes>> mLiveData() {
+        mData = mRepository.mLiveData();
         return mData;
+    }
+
+    public LiveData<List<Heroes>> mLiveDataFav() {
+        if (mDataFav == null) {
+            mDataFav = new MutableLiveData<>();
+        }
+        mDataFav = mRepository.getLiveFavData();
+        return mDataFav;
+    }
+
+    public void getFavData() {
+        mRepository.getFavData();
+
+    }
+
+    public void deleteHero(int id){
+        mRepository.deleteHero(id);
     }
 }
